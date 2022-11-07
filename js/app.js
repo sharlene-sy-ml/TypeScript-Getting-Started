a5732733f0c77cb26786b4c6facb0029a4e0fefe
Sharlene Danica Sy <sharlene_danica_sy@manulife.com>
2022-11-07 21:47:03 +0800

    Config Inheritance and Glob Support in tsconfig.json

--- js/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function startGame() {
-    let messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath. Starting the game.';
-}
-document.getElementById('startGame').addEventListener('click', startGame);
-//# sourceMappingURL=app.js.map
