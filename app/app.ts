282ba031355fa499e4d9b4b34068745f86488082
Sharlene Danica Sy <sharlene_danica_sy@manulife.com>
2022-11-08 21:13:49 +0800

    Module 3 - Writing Better Functions with TypeScript

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,36 @@
 function startGame(){
     //start game
+    let playerName:string|undefined= getInputValue("playername");
+    logPlayer(playerName);
+    postScore(100);
+    postScore(-5);
+
     let messagesElement = document.getElementById('messages')
     messagesElement!.innerText  = 'Welcome to MultiMath. Starting the game.'
 }
 
+function logPlayer(name:string='MultiMath Player' ):void{
+    console.log(`New game starting for plater: ${name}`);
+}
+
+function getInputValue(elementID:string):string | undefined{
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    return inputElement.value===''? undefined : inputElement.value;
+}
+
+function postScore(score:number, playerName:string='MultiMath Player'):void{
+    let logger:(value:string)=>void;
+    logger = score<0 ? logError:logMessage;
+
+    const scoreElement:HTMLElement|null = document.getElementById('postedScores');
+    scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score: ${score}`);
+}
+
 document.getElementById('startGame')!.addEventListener('click',startGame);
+
+const logMessage = (message:string)=>console.log(message);
+function logError(err:string):void{
+    console.error(err);
+}
